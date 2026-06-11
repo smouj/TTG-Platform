c7a874b20655594d67348a989b718075a93b0bb9
smouj <smouj@local>
2026-06-11 16:46:04 +0200

    chore: bump version to 0.6.2

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -8,7 +8,7 @@
 export const SITE_CONFIG = {
   name: "Trading Tazos Game",
   shortName: "TTG",
-  version: "0.6.0",
+  version: "0.6.2",
   canonicalUrl: "https://tradingtazosgame.com",
   totalTazos: 150,
   publishedTazos: 150,
